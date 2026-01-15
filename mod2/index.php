571a046f2029e088b434139df18f889c0195a903
buna212 <bunaberisha27@outlook.com>
2026-01-15 20:31:23 +0100

    push

--- mod2/index.php
+++ mod2/index.php
@@ -1,16 +1,38 @@
 <?php
 
 $emri = "Digital School ";
-echo "I go to  $emri" ;
+echo "I go to  $emri <hr>" ; 
 
 
 $numri = 1.5;
-echo $numri;
+echo $numri."<hr>"; 
 
 
 $x=120;
 $y=50;
 
-echo $x +$y;
+echo $x +$y. "<hr>";
+echo $x -$y. "<hr>";
+
+$emri = "Jora"; 
+
+$mbiemri = "Vula";
+
+$username = "JoraVula";
+
+echo $emri.$mbiemri. "<hr>";
+
+
+$TheString = "Jora Vula";
+ echo strlen ($TheString)."<hr>";
+
+ $TheString1 = "Helllooon jena ne kurs ";
+ echo str_word_count ($TheString1)."<hr>";
+
+  $TheString2 = "Helllooon nuk jemi ne kurs ";
+ echo str_replace ( "nuk","ne", $TheString2)."<hr>";
+
+   $TheString3 = "Programming";
+ echo strrev ( $TheString3)."<hr>";
 
  ?>
---
+++ mod3/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$nr = 4 ;
+
+if ($nr == 0){
+echo "THE NUMBER IS 0";
+}else if ($nr < 0){
+    echo "THE NUMBER IS GREATER THAN 0";
+}
+
+
+
+
+
+
+
+
+
+?>
