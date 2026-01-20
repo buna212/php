fda7b43b97a139e3320d4a005dd899322ba06673
buna212 <bunaberisha27@outlook.com>
2026-01-20 20:49:26 +0100

    m

--- mod3/index.php
+++ mod3/index.php
@@ -1,11 +1,72 @@
-<?php
+ <?php 
 
-$nr = 4 ;
+// $nr = 4 ;
 
-if ($nr == 0){
-echo "THE NUMBER IS 0";
-}else if ($nr < 0){
-    echo "THE NUMBER IS GREATER THAN 0";
+// if ($nr == 0){
+// echo "THE NUMBER IS 0";
+// }else if ($nr < 0){
+//    echo "THE NUMBER IS GREATER THAN 0";
+// }
+
+// Switch
+
+// $day = tuesday;
+
+// switch($day) {
+//        case  "Monday" ;
+//        echo "it is monday,you made a mistake finishing the weekend";
+//        break;
+
+//        case  "Tuesday";
+//        echo "it is tuesday , you finished the worst day"
+//        break;
+
+//             case  "Wednesday";
+//        echo "it is Wednesday , middle of the week"
+//        break;
+
+//             case  "Thursday";
+//        echo "it is Thursday , almost there"
+//        break;
+
+//             case  "Friday";
+//        echo "it is Friday , done for the day"
+//        break;
+
+//             case  "saturday";
+//        echo "it is saturday, free day"
+//        break;
+
+//             case  "Sunday";
+//        echo "it is Sunday , back to monday"
+//        break;
+
+//        default;
+//        echo "error"
+//        break;
+
+
+// }
+
+
+// $numri = 0;
+
+// while($numeri <=5){
+//     echo "the number is $numri";
+//     $numri++;
+
+// }
+
+
+// $numrii = 0
+
+// do {
+//     echo "the number is $numri <hr>" ;
+//     $numrii++;
+// }while($numberii <=5)
+
+for ($x=0; $x<=10){
+    echo "the number is $x";
 }
 
 
@@ -14,6 +75,9 @@ echo "THE NUMBER IS 0";
 
 
 
+ ?>  
 
 
-?>
+
+
+
