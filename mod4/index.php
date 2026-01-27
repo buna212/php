285a9d924cd7d73621239e02ccf13651f5834451
buna212 <bunaberisha27@outlook.com>
2026-01-27 20:38:44 +0100

    mod4

--- mod4/index.php
+++ mod4/index.php
@@ -1,35 +1,97 @@
-<?php
+ <?php
 
 //phpinfo();
 
-$x = "hello";
-$z = 10.3;
-$y = 5 ;
+// $x = "hello";
+// $z = 10.3;
+// $y = 5 ;
 
-print_r($x);
+// print_r($x);
 
-echo gettype ($x);
-echo gettype ($z);
-echo gettype ($y);
+// echo gettype ($x);
+// echo gettype ($z);
+// echo gettype ($y);
 
-function printV() {
-    echo "this is php ".phpversion();
-}
+// function printV() {
+//     echo "this is php ".phpversion();
+// }
 
-printV();
-
-
-function maximum($x ,$y){
- if ($x>$y){
-    return $x;
- }else{
-    return $y;
- }
-
-}
-$test = maximum (100,101);
-echo $test;
+// printV();
 
 
+// function maximum($x ,$y){
+//  if ($x>$y){
+//     return $x;
+//  }else{
+//     return $y;
+//  }
 
-?>
+// }
+// $test = maximum (100,101);
+// echo $test;
+ 
+// function fully_divisable ($n){
+//    if (($n % 2)==0) {
+//       return "$n is fully divisable by 2";
+//    } else {
+//       return "$n is not fully divisable by 2";
+//    } 
+// }
+//  print_r(fully_divisable(67). "<hr>");
+//  print_r(fully_divisable(8). "<hr>");
+//   print_r(fully_divisable(95). "<hr>");
+//  print_r(fully_divisable(55). "<hr>");
+//  print_r(fully_divisable(26). "<hr>");
+//   print_r(fully_divisable(59). "<hr>");
+
+
+// $x = 27; 
+
+// function print_varible() {
+
+
+//    $y = 2;
+//    echo $y;
+// }    
+
+// print_varible();
+// echo $x ;
+
+
+// $sports = array("voleyball" , "gymnastic" , "football" , "handball");
+
+// $fav_color = ["pink" , "purple" , "blue"];
+
+// $nr = [1,2,3,4,5,6,7];
+
+
+// // echo $sport = (2);
+
+// // echo $fav_color = (0);
+
+// echo $nr[5];
+
+// for($i=0;$i<4; $i++){
+// echo $sports[$i],"<br>";
+// }
+
+// array_pop($fav_color);
+
+// var_dump($fav_color);
+
+// array_unshift($fav_color, 'orange')."<br>";
+
+// var_dump($fav_color)."<br>";
+
+// array_shift($fav_color);
+// for($i=0;$i<2; $i++){
+// echo $fav_color[$i],"<br>";
+// }
+// var_dump(array_sum($number));
+
+$numriiiiiiiiii = array(27 , 2 , 11);
+
+$mesi = array_sum($numriiiiiiiiii)/3;
+
+echo $mesi;
+?>  
