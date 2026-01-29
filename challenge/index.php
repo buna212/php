d0c86e6098041f70e1cd200d42dfedbdc674254a
buna212 <bunaberisha27@outlook.com>
2026-01-29 20:16:19 +0100

    index.php

--- challenge/index.php
+++ challenge/index.php
@@ -3,15 +3,17 @@
 $emri = "Jora"; 
 $mbiemri = "Vula";
 
-echo "PËRSHËNDETJE  $emri $mbiemri <hr>";
+echo "PËRSHËNDETJE  $emri $mbiemri ";
 
 
 
 $pi = 3.27;
 $r= 2;
 
-echo  $pi * $r^2.
+echo  $pi * $r^2;
 
+$string7= "hiiii";
+echo strrev($string7);
 
 
 
