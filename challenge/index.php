744647b1e688aebeb0d8538059f35b530e939c30
buna212 <bunaberisha27@outlook.com>
2026-01-29 20:23:30 +0100

    index.php

--- challenge/index.php
+++ challenge/index.php
@@ -13,7 +13,16 @@ $r= 2;
 echo  $pi * $r^2;
 
 $string7= "hiiii";
-echo strrev($string7);
+echo strrev($string7 );
+
+
+$age = 18 ;
+
+ if ($age == 18){
+  echo "JU JENI GATI TE VOTONI";
+}else if ($nr < 18){
+        echo "PERSONI NUK MUND TE VOTON ";
+}
 
 
 
